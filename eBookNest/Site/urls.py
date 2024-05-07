1cf1613e143fdedc185e6d7d4186ffef05395a2c
Hassan Muhammed <142698990+HassanMuhamed1@users.noreply.github.com>
2024-05-07 22:36:04 +0300

    edit all files

--- eBookNest/Site/urls.py
+++ eBookNest/Site/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('delete/', views.delete, name='delete'),
+    path('edit/', views.edit, name='edit'),
+    path('select/', views.select, name='select'),
+    path('home/', views.home, name='select'),
 ]
--- eBookNest/Site/views.py
+++ eBookNest/Site/views.py
@@ -5,4 +5,14 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request,'pages/index.html')
+def add(request):
+    return render(request,'Admin_Home_page/add.html')
+def delete(request):
+    return render(request,'Admin_Home_page/delete.html')
+def edit(request):
+    return render(request,'Admin_Home_page/Edit.html')
+def select(request):
+    return render(request,'Admin_Home_page/Select.html')
+def home(request):
+    return render(request,'home_page/home.html')
