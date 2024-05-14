770ea16fce2989be29a39c434182f42ac38a542d
Hassannetsha <122812098+Hassannetsha@users.noreply.github.com>
2024-05-14 14:57:52 +0300

    draft

--- eBookNest/Site/models.py
+++ eBookNest/Site/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Categorys(models.Model):
+    Category_name = models.CharField(max_length=200, primary_key=True)
+class Book(models.Model):
+    Category = models.ForeignKey(Categorys, on_delete=models.CASCADE, related_name="Books")
