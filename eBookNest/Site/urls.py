2bb6237aa3739ec3c7361bf142ab2488736f6c60
Hassannetsha <122812098+Hassannetsha@users.noreply.github.com>
2024-05-14 12:18:40 +0300

    draft

--- eBookNest/Site/urls.py
+++ eBookNest/Site/urls.py
@@ -3,14 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='index'),
-    # path('SignPages/html/signin-user.html', views.signinUser, name='add'),
-    # path('signup-user.html', views.signupUser, name='delete'),
-    # path('signin-user.html', views.signinUser, name='edit'),
+    path('SignPages/html/signin-user.html', views.signinUser, name='add'),
+    path('signup-user.html', views.signupUser, name='delete'),
+    path('signin-user.html', views.signinUser, name='edit'),
     path('signinadmin.html', views.signinAdmin, name='select1'),
     path('signup-admin.html', views.signupAdmin, name='select2'),
     path('home_page', views.home, name='home'),
-    # path('User_Home_page/availavbleBooks.html', views.available, name='select3'),
-    # path('User_Home_page/borrowedBooks.html', views.borrowed, name='select3'),
+    path('home_page/home.html', views.home, name='home'),
+    path('User_Home_page/availavbleBooks.html', views.available, name='select3'),
+    path('User_Home_page/borrowedBooks.html', views.borrowed, name='select3'),
     path('', views.signout, name='select3'),
 ]
 
