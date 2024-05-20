b7cdf3aef4afda8d0b478804d186cc748eed9c76
Hassannetsha <122812098+Hassannetsha@users.noreply.github.com>
2024-05-20 20:11:57 +0300

    draft 3

--- eBookNest/Site/models.py
+++ eBookNest/Site/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Categorys(models.Model):
-    Category_name = models.CharField(max_length=200, primary_key=True)
+    Category_name = models.CharField(max_length=200)
+    Category_Id = models.AutoField(primary_key=True)
     def __str__(self):
         return self.Category_name
 
