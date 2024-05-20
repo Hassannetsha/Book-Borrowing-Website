b0a291c33e2c86b09f4a416a22429c076c6e638d
Hassannetsha <122812098+Hassannetsha@users.noreply.github.com>
2024-05-20 23:35:01 +0300

    added one book and edited some data base

--- eBookNest/Site/models.py
+++ eBookNest/Site/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class Categorys(models.Model):
     Category_name = models.CharField(max_length=200)
-    Category_Id = models.AutoField(primary_key=True)
     def __str__(self):
         return self.Category_name
 
@@ -13,7 +12,6 @@ class Book(models.Model):
     no_page = models.IntegerField()
     BookCover = models.ImageField(upload_to='book_covers/' ,null=True , blank =True )
     Category = models.ForeignKey(Categorys, on_delete=models.CASCADE, related_name="Books")
-    Book_Id = models.IntegerField(primary_key=True)
     description = models.TextField()
     Available = models.BooleanField(default=True)
     def __str__(self):
