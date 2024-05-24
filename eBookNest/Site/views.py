4013d2bb54b409f19078d7f224172c9263e90f0b
Mohannad <137307107+Honda-6@users.noreply.github.com>
2024-05-24 05:58:45 +0300

    Final Tmam gedaaan

--- eBookNest/Site/views.py
+++ eBookNest/Site/views.py
@@ -8,11 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 
 
-
-
 def index(request):
-    return render(request, 'main-sign-page.html')
-
     return render(request, 'main-sign-page.html')
 
 
@@ -20,13 +16,7 @@ def signinAdmin(request):
     return render(request, 'signinadmin.html')
 
 
-    return render(request, 'signinadmin.html')
-
-
 def signUpAdmin(request):
-    return render(request, 'signup-admin.html')
-
-
     return render(request, 'signup-admin.html')
 
 
@@ -36,19 +26,11 @@ def signinUser(request):
     return render(request, 'signin-user.html', {'users': users})
 
 
-
-    return render(request, 'signin-user.html', {'users': users})
-
-
 def signUpUser(request):
-    return render(request, 'signup-user.html')
     return render(request, 'signup-user.html')
 # def test(request):
 #     books = Book.objects.all()  # or any other queryset
 #     return render(request, 'test.html', {'books': books})
-
-
-def home(request, Id):
 
 
 def home(request, Id):
@@ -57,19 +39,15 @@ def home(request, Id):
     user = get_object_or_404(User, pk=Id)
     return render(request, 'home.html', {'books': books, 'categories': categories, "user": user})
 
-    return render(request, 'home.html', {'books': books, 'categories': categories, "user": user})
-
 
 def category_data(request):
     categories = list(Categorys.objects.values())
     return JsonResponse(categories, safe=False)
 
 
-
 def book_data(request):
     books = list(Book.objects.values())
     return JsonResponse(books, safe=False)
-
 
 
 def Convert(request, Id):
@@ -78,9 +56,7 @@ def Convert(request, Id):
         book.Available = not book.Available
         book.save()
         return JsonResponse({'status': 'Book updated'}, status=200, content_type='application/json')
-        return JsonResponse({'status': 'Book updated'}, status=200, content_type='application/json')
     return JsonResponse({'status': 'Invalid request'}, status=400)
-
 
 
 def delete_book(request, book_id):
@@ -90,7 +66,9 @@ def delete_book(request, book_id):
         return JsonResponse({'status': 'Book deleted'})
     return JsonResponse({'status': 'Invalid request'}, status=400)
 
-
+def deletePage(request,userID):
+    user = get_object_or_404(User,pk=userID)
+    return render(request,'delete.html',{'user':user})
 
 def getbooks(request):
     books = Book.objects.all()
@@ -100,11 +78,6 @@ def getbooks(request):
 
 
 def Availableallbooks(request, userId):
-    return JsonResponse({"books": list(books.values()),
-                         "categories": list(Categoryies.values())}, status=200)
-
-
-def Availableallbooks(request, userId):
     books = Book.objects.all()
     categories = Categorys.objects.all()
     user = get_object_or_404(User, pk=userId)
@@ -112,16 +85,8 @@ def Availableallbooks(request, userId):
 
 
 def Borrowedbooks(request, userId):
-    return render(request, 'availavbleBooks.html', {'books': books, 'categories': categories, "user": user})
-
-
-def Borrowedbooks(request, userId):
     books = Book.objects.all()
     user = get_object_or_404(User, pk=userId)
-    return render(request, 'borrowedBooks.html', {'books': books, "user": user})
-
-
-def selectBook(request, userId):
     return render(request, 'borrowedBooks.html', {'books': books, "user": user})
 
 
@@ -137,8 +102,6 @@ def selectBook(request, userId):
 #     return render(request, 'bookdetails.html', {"book": book, "category": category,"user":user})
 
 
-def Bookdetails(request, bookId, userId):
-    return render(request, 'Select.html', {'books': books, 'categories': categories, "user": user})
 # def Details(request, bookId, userId):
 #     book = get_object_or_404(Book, pk=bookId)
 #     category = book.Category
@@ -146,19 +109,16 @@ def Bookdetails(request, bookId, userId):
 #     return render(request, 'bookdetails.html', {"book": book, "category": category,"user":user})
 
 
-def Bookdetails(request, bookId, userId):
+def Bookdetails(request, userId, bookId):
     book = get_object_or_404(Book, pk=bookId)
     category = book.Category
     user = get_object_or_404(User, pk=userId)
-    return render(request, 'bookdetails.html', {"book": book, "category": category, "user": user})
     return render(request, 'bookdetails.html', {"book": book, "category": category, "user": user})
 # def toggleusertype(request,ID):
 #     if request.method == "PATCH":
 #         return JsonResponse({'status': 'user updated'}, status=200, content_type='application/json')
 #         return JsonResponse({'status': 'user updated'}, status=200, content_type='application/json')
 #     return JsonResponse({'status': 'Invalid request'}, status=400)
-
-
 
 
 @csrf_exempt
@@ -181,29 +141,21 @@ def save_user(request):
             return JsonResponse({'status': 'success'})
 
 
-
-
 def get_users_json(request):
     user = User.objects.all()
     user_json = serializers.serialize('json', user)
     return JsonResponse(user_json, safe=False)
 
 
-def EditBook(request, Id):
-    book = get_object_or_404(Book, pk=Id)
+def EditBook(request, Id,Id2):
+    book = get_object_or_404(Book, pk=Id2)
     category = get_object_or_404(Categorys, pk=book.Category_id)
-    return render(request, 'Edit.html', {'book': book, 'category': category})
-
-def EditBook(request, Id):
-    book = get_object_or_404(Book, pk=Id)
-    category = get_object_or_404(Categorys, pk=book.Category_id)
-    return render(request, 'Edit.html', {'book': book, 'category': category})
-    # return render(request,'Edit.html',{'book':book,"user":user})
+    user = get_object_or_404(User,pk=Id)
+    return render(request, 'Edit.html', {'book': book, 'category': category, 'user':user})
 
 
 def addBook(request, userId):
     user = get_object_or_404(User, pk=userId)
-    print(5)
     return render(request, "add.html", {'user': user})
 
 
@@ -211,19 +163,18 @@ def addBook(request, userId):
 def save_book(request):
     if request.method == 'POST':
         try:
-            data = json.loads(request.POST['str'])
-            if Book.objects.filter(pk=data.get('ID')).count():
+            data = json.loads(request.POST['json'])
+            if Book.objects.filter(pk=data.get('ID')):
                 return JsonResponse({'status': 'failed', 'message': 'Id already exists'})
             cat = data.get('Category')
             zeftID = 0
             catid = Categorys.objects.filter(Category_name=cat)
-            if not catid.count():
+            if not catid:
                 catid = Categorys.objects.create(
                     Category_name=cat
                 )
                 catid.save()
                 zeftID = catid.id
-                print(zeftID)
             else:
                 zeftID = catid[0].id
             newBook = Book.objects.create(
@@ -245,23 +196,17 @@ def save_book(request):
 def modifyBook(request):
     if request.method == 'POST':
         try:
-            data = json.loads(request.POST['str'])
+            data = json.loads(request.POST['json'])
+            oldCat = Categorys.objects.filter(Category_name=Book.objects.get(pk=data.get('ID')).Category)
             cat = data.get('Category')
             zeftID = 0
             catid = Categorys.objects.filter(Category_name=cat)
-            if not catid.count():
-                q = Categorys.objects.get(
-                    Category_name=Categorys.objects.last())
-                if q is None:
-                    q = 1
-                else:
-                    q = q.id + 1
-
+            if not catid:
                 catid = Categorys.objects.create(
-                    id=q,
                     Category_name=cat
                 )
                 catid.save()
+                zeftID = catid.id
             else:
                 zeftID = catid[0].id
             newBook = Book.objects.get(pk=data.get('ID'))
@@ -272,6 +217,9 @@ def modifyBook(request):
             newBook.Category = Categorys.objects.get(pk=zeftID)
             newBook.description = data.get('Description')
             newBook.save()
+            if not Book.objects.filter(Category=oldCat[0]):
+                print('SOBRIIIII')
+                Categorys.objects.filter(Category_name=oldCat[0])[0].delete()
             return JsonResponse({'status': 'success', 'message': 'Book edited Successfully'})
         except Exception as e:
             print(e)
